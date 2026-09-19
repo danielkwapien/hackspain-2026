de5ad1d592ce0652e7f83b89bff9a2038118d928
alfonsomayoral <alfonsomayoral29@gmail.com>
2026-09-19 17:57:14 +0200

    XR-034: gate the widget body with its own red test

--- evals/checks/XR-034.sh
+++ evals/checks/XR-034.sh
@@ -5,5 +5,6 @@ web_test charts/treemap-columns
 web_test charts/treemap-fit
 web_test charts/Treemap
 web_test charts/TreemapLayout
-web_test widgets/treemap
+web_test widgets/treemap/TreemapColumns
+web_test widgets/treemap/TreemapWidget
 api_json '/api/v2/treemap?group_by=country&metric=score&size_by=n_companies' '.group_by == "country" and .size_by == "n_companies" and (.groups | length) > 0'
